253b007bd8aab0f6f67397aa934035e52d544bab
Gara <zhuo_ying@kingdee.com>
2018-07-30 14:12:09 +0800

    update

--- src/main/java/com/gara/hellodemo/HelloDemoApplication.java
+++ src/main/java/com/gara/hellodemo/HelloDemoApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HellodemoApplication {
+public class HelloDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HellodemoApplication.class, args);
+		SpringApplication.run(HelloDemoApplication.class, args);
 	}
 }
